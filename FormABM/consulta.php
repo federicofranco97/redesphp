c5402f452f93ab81c5b301bdd3b61da91dd95529
federico franco- note <43964230+federicofranco97@users.noreply.github.com>
2019-10-28 22:32:53 -0300

    added form abm base

---
+++ FormABM/consulta.php
@@ -0,0 +1,19 @@
+<?php
+    $orden = $_POST["OrderBy"];
+    sleep(3);
+    $mysqli = new mysqli("localhost","root","","ubd1");
+    $query = "select * from empleado order by ".$orden;
+    $resultado = $mysqli->query($query);
+    $CantidadRegistros = $resultado->num_rows;  
+    $almacenSql = [];
+    while($fila = $resultado->fetch_assoc()){
+        $objSql = new stdclass;   
+        $objSql-> IdUsuario = $fila["IdUsuario"];
+        $objSql-> NombreUsuario = $fila["NombreUsuario"];
+        $objSql-> LoginUsuario = $fila["LoginUsuario"];
+        $objSql-> PasswordUsuario = $fila["PasswordUsuario"];
+        array_push($almacenSql,$objSql);
+    }
+    $JsonObj = Json_encode($almacenSql);
+    echo $JsonObj;
+?>
